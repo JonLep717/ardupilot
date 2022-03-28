5f26701ab35fc5d9ec458b46b024b6236864358e
Tristan White <john.white66@tamu.edu>
2022-03-28 10:16:51 -0500

    Test Change
    test

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -9,6 +9,8 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
+	Test Change
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
